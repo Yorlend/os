2a7ce4f7878d172e6c8ec3cc19b058a20e37c417
yorlend <yorlend@yandex.ru>
2023-02-27 13:08:58 +0300

    lab2 denorm

--- sem6/lab_02/recvsend/client.c
+++ sem6/lab_02/recvsend/client.c
@@ -1,60 +1,57 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #define SERVER_SOCKET_PATH "./server.sock"
 #define CLIENT_SOCKET_PATH "./client.sock"
 
-void send_request(int sock)
+int main(void)
 {
+    int fd;
     struct sockaddr_un addr = {
         .sun_family = AF_UNIX,
         .sun_path = SERVER_SOCKET_PATH
     };
 
     char buf[256];
-    snprintf(buf, sizeof(buf), "request from pid=%d", getpid());
 
-    if (sendto(sock, buf, sizeof(buf), 0, (struct sockaddr*)&addr, sizeof(addr)) == -1)
-    {
-        printf("error when sending request message\n");
-        return;
-    }
-
-    printf("sent request from pid=%d\n", getpid());
-
-    if (recvfrom(sock, buf, sizeof(buf), 0, NULL, NULL) == -1)
-        printf("error when receiving response\n");
-    else
-        printf("received response: \"%s\"\n", buf);
-}
-
-int main(void)
-{
-    int sock = socket(AF_UNIX, SOCK_DGRAM, 0);
-
-    struct sockaddr_un addr = {
+    struct sockaddr_un client_addr = {
         .sun_family = AF_UNIX,
         .sun_path = CLIENT_SOCKET_PATH
     };
 
-    if (sock == -1)
+    fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (fd == -1)
     {
-        printf("failed to create socket\n");
-        return -1;
+        perror("socket");
+        exit(1);
     }
-    else if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+    else if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
     {
-        printf("bind error\n");
+        perror("bind");
     }
     else
     {
         printf("sending request...\n");
-        send_request(sock);
+        snprintf(buf, sizeof(buf), "request from pid=%d", getpid());
+
+        if (sendto(fd, buf, sizeof(buf), 0, (struct sockaddr*)&client_addr, sizeof(addr)) == -1)
+        {
+            perror("sendto");
+            exit(1);
+        }
+
+        printf("sent request from pid=%d\n", getpid());
+
+        if (recvfrom(fd, buf, sizeof(buf), 0, NULL, NULL) == -1)
+            perror("recvfrom");
+        else
+            printf("received response: \"%s\"\n", buf);
     }
 
-    close(sock);
+    close(fd);
     remove(CLIENT_SOCKET_PATH);
     return 0;
 }
--- sem6/lab_02/recvsend/server.c
+++ sem6/lab_02/recvsend/server.c
@@ -2,62 +2,57 @@
 #include <signal.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SOCKET_PATH "./server.sock"
 
-int sock;
+int fd;
 
 void sigint_handler(int sig);
 
-void listen_loop(int sock)
-{
-    char buf[256];
-    struct sockaddr client_addr;
-    socklen_t len;
-
-    while (1)
-    {
-
-        if (recvfrom(sock, buf, 256, 0, &client_addr, &len) == -1)
-        {
-            printf("error when receiving request\n");
-            return;
-        }
-
-        printf("received request: \"%s\"\n", buf);
-        int pid = atoi(buf + 17);
-
-        snprintf(buf, sizeof(buf), "response for pid=%d", pid);
-
-        if (sendto(sock, buf, sizeof(buf), 0, &client_addr, len) == -1)
-            printf("error when sending response for pid=%d\n", pid);
-        else
-            printf("sent response for pid=%d\n", pid);
-    }
-}
-
 int main(void)
 {
+    signal(SIGINT, sigint_handler);
+
     struct sockaddr_un addr = {
         .sun_family = AF_UNIX,
         .sun_path = SOCKET_PATH
     };
 
-    signal(SIGINT, sigint_handler);
+    char buf[256];
+    struct sockaddr client_addr;
+    socklen_t len;
 
-    sock = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if (sock == -1)
-        printf("failed to create socket\n");
-    else if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+    fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (fd == -1)
+        perror("socket");
+    else if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
         printf("cannot bind\n");
     else
     {
         printf("bind successful. Ready to listen...\n");
 
-        listen(sock, 1);
-        listen_loop(sock);
+        listen(fd, 1);
+        while (1)
+        {
+            if (recvfrom(fd, buf, 256, 0, &client_addr, &len) == -1)
+            {
+                perror("recvfrom");
+                exit(1);
+            }
+
+            printf("received request: \"%s\"\n", buf);
+            int pid = atoi(buf + 17);
+
+            snprintf(buf, sizeof(buf), "response for pid=%d", pid);
+
+            if (sendto(fd, buf, sizeof(buf), 0, &client_addr, len) == -1)
+                printf("error when sending response for pid=%d\n", pid);
+            else
+                printf("sent response for pid=%d\n", pid);
+        }
     }
 
     return -1;
@@ -65,7 +60,7 @@ int main(void)
 
 void sigint_handler(int sig)
 {
-    close(sock);
+    close(fd);
     remove(SOCKET_PATH);
     printf("\nRemoved socket file.\n");
     exit(0);
